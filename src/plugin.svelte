af06bc2c3a89829f78e6afb4f08e0aa434398c54
Rasul Adasov <rasul@futureblur.com>
2024-11-18 14:37:30 +0100

    Initial commit

---
+++ src/plugin.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import * as pckg from "../package.json";
+	import "./app.css"
+	
+	
+	interface Props {
+		// Inputs
+		doc: XMLDocument | undefined;
+		editCount: number;
+	}
+
+	let { doc, editCount }: Props = $props();
+</script>
+
+{#if doc}
+	<h2>Welcome to new-oscd-plugin</h2>
+{/if}
+
+<input type="hidden" name="package-name" value={pckg.name} />
+<input type="hidden" name="package-version" value={pckg.version} />
